2be87f5a72ac59ee8c531cd9673b6580d75f0fe3
Thomas Nelson <tanelso2@gmail.com>
2017-10-24 17:15:27 -0700

    Got a little further. Next section looks complicated and I'm tired.
    Should pick up tomorrow maybe?

--- parsing.hs
+++ parsing.hs
@@ -287,8 +287,8 @@ numericBinop op params = do
 
 boolBinop :: (LispVal -> ThrowsError a) -> (a -> a -> Bool) -> [LispVal] -> ThrowsError LispVal
 boolBinop unpacker op (left:right:[]) = do
-                                          left <- unpacker $ left
-                                          right <- unpacker $ right
+                                          left <- unpacker left
+                                          right <- unpacker right
                                           return $ Bool $ left `op` right
 boolBinop unpacker op args = throwError $ NumArgs 2 args
 
@@ -314,6 +314,40 @@ unpackNum (String s) = let parsed = reads s in
                                 else return $ fst $ parsed !! 0
 unpackNum (List [n]) = unpackNum n
 unpackNum notNum = throwError $ TypeMismatch "number" notNum
+
+car :: [LispVal] -> ThrowsError LispVal
+car [List (x : xs)] = return x
+car [DottedList (x : xs) _] = return x
+car [badArg] = throwError $ TypeMismatch "pair" badArg
+car badArgList = throwError $ NumArgs 1 badArgList
+
+cdr :: [LispVal] -> ThrowsError LispVal
+cdr [List (x : xs)] = return $ List xs
+cdr [DottedList [_] x] = return x
+cdr [DottedList (_ : xs) x] = return $ DottedList xs x
+cdr [badArg] = throwError $ TypeMismatch "pair" badArg
+cdr badArgList = throwError $ NumArgs 1 badArgList
+
+cons :: [LispVal] -> ThrowsError LispVal
+cons [x1, List []] = return $ List [x1]
+cons [x, List xs] = return $ List $ x : xs
+cons [x, DottedList xs xlast] = return $ DottedList (x : xs) xlast
+cons [x1, x2] = return $ DottedList [x1] x2
+cons badArgList = throwError $ NumArgs 2 badArgList
+
+eqv :: [LispVal] -> ThrowsError LispVal
+eqv [(Bool arg1), (Bool arg2)] = return $ Bool $ arg1 == arg2
+eqv [(Number arg1), (Number arg2)] = return $ Bool $ arg1 == arg2
+eqv [(String arg1), (String arg2)] = return $ Bool $ arg1 == arg2
+eqv [(Atom arg1), (Atom arg2)] = return $ Bool $ arg1 == arg2
+eqv [(DottedList xs x), (DottedList ys y)] = eqv [List $ xs ++ [x], List $ ys ++ [y]]
+eqv [(List arg1), (List arg2)] = return $ Bool $ (length arg1 == length arg2) &&
+    (all eqvPair $ zip arg1 arg2)
+        where eqvPair (x1, x2) = case eqv [x1, x2] of
+                                    Left err -> False
+                                    Right (Bool val) -> val
+eqv [_, _] = return $ Bool False
+eqv badArgList = throwError $ NumArgs 2 badArgList
 
 main :: IO ()
 main = do
